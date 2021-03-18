b8f4fff81929aac488251c2e42b666499b25d387
Joseph Soukup <u17.426@pslib.cloud>
2021-03-18 13:44:43 +0100

    web image upload, desktop RGD and gray filter

---
+++ FakeDesktopGimp/FakeGimp/FakeGimp/Model/Filters.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace FakeGimp.Model
+{
+    class Filters
+    {
+        public BitmapImage image { get; set; }
+        private BitmapImage original_image;
+        private int[,] array;
+        private int width, height;
+        /*
+        public void ChangImage(BitmapImage image)
+        {
+            image = this.image = image;
+            width = image.PixelWidth;
+            height = image.PixelHeight;
+        }
+        */
+
+        public BitmapImage GrayScale(BitmapImage image)
+        {
+            if (image != null)
+            {
+                array = BitmapImageToArray2D(image);
+                for (int x = 0; x < image.PixelHeight; x++)
+                {
+                    for (int y = 0; y < image.PixelWidth; y++)
+                    {
+                        int red = ((array[x, y] & 0x00FF0000) >> 16);
+                        int green = ((array[x, y] & 0x0000FF00) >> 8);
+                        int blue = (array[x, y] & 0x000000FF);
+                        int prumer = (int)(red * 0.3 + green * 0.59 + blue * 0.11) / 3;
+                        array[x, y] = (int)(array[x, y] & 0xFF000000) + prumer + (prumer << 8) + (prumer << 16);
+
+                    }
+                }
+                return ConvertWriteableBitmapToBitmapImage(Array2DToWriteableBitmap(array, image));
+            }
+            return null;
+        }
+        public BitmapImage Shades(int channel, BitmapImage image)
+        {
+            if (image != null)
+            {
+                Stopwatch timer = new Stopwatch();
+                uint maska = 0;
+                array = BitmapImageToArray2D(image);
+                timer.Start();
+                switch (channel)
+                {
+                    case 0: maska = 0xFFFF0000; break;
+                    case 1: maska = 0xFF00FF00; break;
+                    case 2: maska = 0xFF0000FF; break;
+                }
+                for (int x = 0; x < image.PixelHeight; x++)
+                {
+                    for (int y = 0; y < image.PixelWidth; y++)
+                    {
+                        array[x, y] &= (int)maska;
+                    }
+
+                }
+                timer.Stop();
+                Console.WriteLine("čas na jedno vlákno: " + timer.ElapsedMilliseconds);
+                return ConvertWriteableBitmapToBitmapImage(Array2DToWriteableBitmap(array, image));
+            }
+            return null;
+        }
+        public static int[,] BitmapImageToArray2D(BitmapImage src)
+        {
+            int[,] array2D = new int[src.PixelHeight, src.PixelWidth];
+
+            WriteableBitmap wb = new WriteableBitmap(src);
+            int width = wb.PixelWidth;
+            int height = wb.PixelHeight;
+            int bytesPerPixel = (wb.Format.BitsPerPixel + 7) / 8;
+            int stride = wb.BackBufferStride;
+            wb.Lock();
+            unsafe
+            {
+                byte* pImgData = (byte*)wb.BackBuffer;
+                int cRowStart = 0;
+                int cColStart = 0;
+                for (int row = 0; row < height; row++)
+                {
+                    cColStart = cRowStart;
+                    for (int col = 0; col < width; col++)
+                    {
+                        byte* bPixel = pImgData + cColStart;
+                        //bPixel[0] // Blue
+                        //bPixel[1] // Green
+                        //bPixel[2] // Red
+                        int pixel = bPixel[2]; //Red
+                        pixel = (pixel << 8) + bPixel[1]; //Green
+                        pixel = (pixel << 8) + bPixel[0]; //Blue
+                        array2D[row, col] = pixel;
+
+                        cColStart += bytesPerPixel;
+                    }
+                    cRowStart += stride;
+                }
+            }
+            wb.Unlock();
+            wb.Freeze();
+
+            return array2D;
+        }
+
+        public static WriteableBitmap Array2DToWriteableBitmap(int[,] array2D, BitmapImage src)
+        {
+            WriteableBitmap wb = new WriteableBitmap(src);
+            int width = wb.PixelWidth;
+            int height = wb.PixelHeight;
+            int bytesPerPixel = (wb.Format.BitsPerPixel + 7) / 8;
+            int stride = wb.BackBufferStride;
+            wb.Lock();
+            unsafe
+            {
+                byte* pImgData = (byte*)wb.BackBuffer;
+                int cRowStart = 0;
+                int cColStart = 0;
+                for (int row = 0; row < height; row++)
+                {
+                    cColStart = cRowStart;
+                    for (int col = 0; col < width; col++)
+                    {
+                        byte* bPixel = pImgData + cColStart;
+
+                        bPixel[0] = (byte)((array2D[row, col] & 0xFF));// Blue
+                        bPixel[1] = (byte)((array2D[row, col] & 0xFF00) >> 8);// Green
+                        bPixel[2] = (byte)((array2D[row, col] & 0xFF0000) >> 16);// Red
+
+                        cColStart += bytesPerPixel;
+                    }
+                    cRowStart += stride;
+                }
+            }
+            wb.Unlock();
+            wb.Freeze();
+
+            return wb;
+        }
+
+        public static BitmapImage ConvertWriteableBitmapToBitmapImage(WriteableBitmap wbm)
+        {
+            BitmapImage bmImage = new BitmapImage();
+            using (MemoryStream stream = new MemoryStream())
+            {
+                PngBitmapEncoder encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(wbm));
+                encoder.Save(stream);
+                bmImage.BeginInit();
+                bmImage.CacheOption = BitmapCacheOption.OnLoad;
+                bmImage.StreamSource = stream;
+                bmImage.EndInit();
+                bmImage.Freeze();
+            }
+            return bmImage;
+        }
+
+
+    }
+}
---
+++ FakeDesktopGimp/FakeGimp/FakeGimp/ViewModel/MainViewModel.cs
@@ -0,0 +1,77 @@
+﻿using FakeGimp.Model;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace FakeGimp.ViewModel
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        private BitmapImage image;
+        public BitmapImage Image
+        {
+            get => image;
+            set
+            {
+                image = value; NotifyPropertyChanged();
+            }
+        }
+
+        private Filters filters { get; set; }
+        public Command LoadImage { get; set; }
+        public Command GrayScale { get; set; }
+        public Command OdstinyCervene { get; set; }
+        public Command OdstinyZelene { get; set; }
+        public Command OdstinyModre { get; set; }
+        
+        
+        public MainViewModel()
+        {
+            filters = new Filters();
+            LoadImage = new Command(
+                () => { LoadImageExecute(); },
+                () => { return true; }
+            );
+            GrayScale = new Command(
+                () => { Image = filters.GrayScale(Image); },
+                () => { return true; }
+            );
+            OdstinyCervene = new Command(
+                () => { Image = filters.Shades(0, Image); },
+                () => { return true; }
+            );
+            OdstinyZelene = new Command(
+                () => { Image = filters.Shades(1, Image); },
+                () => { return true; }
+            );
+            OdstinyModre = new Command(
+                () => { Image = filters.Shades(2, Image); },
+                () => { return true; }
+            );
+
+        }
+
+        public async void LoadImageExecute()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Obrázky (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+            var result = dialog.ShowDialog();
+            BitmapImage imgSource = new BitmapImage(new Uri(dialog.FileName));
+            Image = imgSource;
+        }
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+}
