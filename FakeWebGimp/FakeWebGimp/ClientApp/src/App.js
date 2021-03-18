e3b3959e5d96411b3f955d38cf4bf3eed881fe96
Joseph Soukup <u17.426@pslib.cloud>
2021-03-18 13:52:09 +0100

    gitignore and web app

---
+++ FakeWebGimp/FakeWebGimp/ClientApp/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import "./style.css";
+
+const App = () => {
+
+    return (
+        <>
+            <div class="intro">
+                <h1>WEB GIMP</h1>
+            </div>
+            <section>
+                <h1>Drag or select image</h1>
+            </section>
+        </>
+    );
+}
+export default App;
